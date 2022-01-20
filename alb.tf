4e2a3ea10fb20a1e52355e1de3a48e6881be2222
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-20 06:45:47 +0530

    Update terraform-mutable-app-module/

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "tg" {
   name     = "${var.ENV}-${var.COMPONENT}"
-  port     = 80
+  port     = var.APP_PORT
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
 }
