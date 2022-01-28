1153ed9549c931cf9182bda783bcfb1c92c9bfcf
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-01-28 08:31:06 +0530

    Update route53.tf

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "private-record" {
   count   = var.LB_PRIVATE ? 1 : 0
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
+  name    = "${var.COMPONENT}-${var.ENV}.ajitdevops-internal"
   type    = "CNAME"
   ttl     = "300"
   records = [data.terraform_remote_state.alb.outputs.PRIVATE_LB_DNSNAME]
@@ -10,7 +10,7 @@ resource "aws_route53_record" "private-record" {
 //resource "aws_route53_record" "public-record" {
 //  count   = var.LB_PUBLIC ? 1 : 0
 //  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-//  name    = "${var.COMPONENT}-${var.ENV}.roboshop.internal"
+//  name    = "${var.COMPONENT}-${var.ENV}.ajitdevops-internal"
 //  type    = "CNAME"
 //  ttl     = "300"
 //  records = [data.terraform_remote_state.vpc.outputs.PRIVATE_LB_DNSNAME]
