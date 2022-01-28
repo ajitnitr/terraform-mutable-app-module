707b433f9950638250c635b24aa247e6ca93ba85
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-01-28 12:16:08 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -7,7 +7,7 @@ data "aws_ami" "ami" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "terraform-b61"
+    bucket = "terraform-bkt2022"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
@@ -16,7 +16,7 @@ data "terraform_remote_state" "vpc" {
 data "terraform_remote_state" "alb" {
   backend = "s3"
   config = {
-    bucket = "terraform-b61"
+    bucket = "terraform-bkt2022"
     key    = "mutable/alb/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
   }
